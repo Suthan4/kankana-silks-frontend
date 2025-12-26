36e024d05c2f162a92087614ec872f063944d33f
Suthan4 <harisuthan268@gmail.com>
2025-12-26 21:45:23 +0530

    updated landing pg

--- src/components/landingPage.tsx
+++ src/components/landingPage.tsx
@@ -1,6 +1,11 @@
-import LandingPageClient from "./landingPage.client";
+import Collection from "./sections/collections";
+import CTASection from "./sections/cta-section";
 import HeroSection from "./sections/hero-section";
+import HorizontalShowcase from "./sections/HorizontalShowcase";
+import PremiumCollectionCarousel from "./sections/premium-collection";
 import RealCollection from "./sections/real-collection";
+import SquareCollectionsCarousel from "./sections/squareCollectionCarousel";
+import VerticalParallaxHero from "./sections/verticalParallaxScroll";
 
 export default function LandingPage() {
   return (
@@ -10,8 +15,12 @@ export default function LandingPage() {
       <RealCollection />
 
       {/* Lazy-loaded CSR */}
-      <LandingPageClient />
-
+      <HorizontalShowcase />
+      <PremiumCollectionCarousel />
+      <VerticalParallaxHero />
+      <SquareCollectionsCarousel />
+      <Collection />
+      <CTASection />
     </>
   );
 }
