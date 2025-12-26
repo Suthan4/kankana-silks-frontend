710d1156ddac1cd713b45217949b472a37082ae0
Suthan4 <harisuthan268@gmail.com>
2025-12-26 19:35:52 +0530

    updated

--- src/components/landingPage.client.tsx
+++ src/components/landingPage.client.tsx
@@ -1,33 +1,39 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import HorizontalShowcase from "./sections/HorizontalShowcase";
+import PremiumCollectionCarousel from "./sections/premium-collection";
+import VerticalParallaxHero from "./sections/verticalParallaxScroll";
+import SquareCollectionsCarousel from "./sections/squareCollectionCarousel";
+import Collection from "./sections/collections";
+import CTASection from "./sections/cta-section";
 
-const HorizontalShowcase = dynamic(
-    () => import("./sections/HorizontalShowcase"),
-    { ssr: false }
-);
+// const HorizontalShowcase = dynamic(
+//     () => import("./sections/HorizontalShowcase"),
+//     { ssr: false }
+// );
 
-const PremiumCollectionCarousel = dynamic(
-    () => import("./sections/premium-collection"),
-    { ssr: false }
-);
+// const PremiumCollectionCarousel = dynamic(
+//     () => import("./sections/premium-collection"),
+//     { ssr: false }
+// );
 
-const VerticalParallaxHero = dynamic(
-    () => import("./sections/verticalParallaxScroll"),
-    { ssr: false }
-);
+// const VerticalParallaxHero = dynamic(
+//     () => import("./sections/verticalParallaxScroll"),
+//     { ssr: false }
+// );
 
-const SquareCollectionsCarousel = dynamic(
-    () => import("./sections/squareCollectionCarousel"),
-    { ssr: false }
-);
+// const SquareCollectionsCarousel = dynamic(
+//     () => import("./sections/squareCollectionCarousel"),
+//     { ssr: false }
+// );
 
-const Collection = dynamic(() => import("./sections/collections"), {
-    ssr: false,
-});
-const CTASection = dynamic(() => import("./sections/cta-section"), {
-  ssr: false,
-});
+// const Collection = dynamic(() => import("./sections/collections"), {
+//     ssr: false,
+// });
+// const CTASection = dynamic(() => import("./sections/cta-section"), {
+//   ssr: false,
+// });
 
 export default function LandingPageClient() {
   return (
