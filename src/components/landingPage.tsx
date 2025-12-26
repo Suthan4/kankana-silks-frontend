75c24f16904f282360a61b508b8f562881cef8c8
Suthan4 <harisuthan268@gmail.com>
2025-12-26 17:32:41 +0530

    updated landing page to static

--- src/components/landingPage.tsx
+++ src/components/landingPage.tsx
@@ -1,10 +1,7 @@
-"use client";
+export const dynamic = "force-static";
 
 import Collection from "./sections/collections";
-import CollectionsSection from "./sections/collections-section";
 import CTASection from "./sections/cta-section";
-import FeaturedSection from "./sections/featured-section";
-import Hero from "./sections/hero-section";
 import HeroSection from "./sections/hero-section";
 import HorizontalShowcase from "./sections/HorizontalShowcase";
 import PremiumCollectionCarousel from "./sections/premium-collection";
